f509b090261a8fe32e79da29f232bc9af86861ab
Antonio Marcos dos Santos de Souza <71250901+Antonio-Marcos86@users.noreply.github.com>
2021-04-26 09:55:19 -0300

    Inserindo novas funcionalidades

---
+++ app/src/main/java/com/br/ifoodclone/model/Usuario.java
@@ -0,0 +1,72 @@
+package com.br.ifoodclone.model;
+
+import com.br.ifoodclone.helpers.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+
+public class Usuario {
+
+    private String idUsuario;
+    private String urlImagem;
+    private String nome;
+    private String endereco;
+    private String cep;
+    private String cidade;
+    public Usuario() {
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void salvar(){
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebase();
+        DatabaseReference usuarioRef = firebaseRef.child("usuario").child(getIdUsuario());
+        usuarioRef.setValue(this);
+
+
+    }
+}
