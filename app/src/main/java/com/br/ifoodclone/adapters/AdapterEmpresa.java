529677af86c3282dc95cc437658d0113136b8dec
Antonio Marcos dos Santos de Souza <71250901+Antonio-Marcos86@users.noreply.github.com>
2021-04-30 14:52:08 -0300

    Update AdapterEmpresa.java

--- app/src/main/java/com/br/ifoodclone/adapters/AdapterEmpresa.java
+++ app/src/main/java/com/br/ifoodclone/adapters/AdapterEmpresa.java
@@ -31,8 +31,8 @@ public class AdapterEmpresa extends RecyclerView.Adapter<AdapterEmpresa.MyViewHo
         Empresa empresa = empresas.get(i);
         holder.nomeEmpresa.setText(empresa.getNome());
         holder.categoria.setText(empresa.getCategoria() + " - ");
-        holder.tempo.setText(empresa.getTempo() + " Min");
-        holder.entrega.setText("R$ " + empresa.getPrecoEntrega().toString());
+        holder.tempo.setText(empresa.getTempo() + " Min - ");
+        holder.entrega.setText("entrega R$ " + empresa.getPrecoEntrega().toString());
 
         //Carregar imagem
         String urlImagem = empresa.getUrlImagem();
